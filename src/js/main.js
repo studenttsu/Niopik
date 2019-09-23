a3d71509c45a6559c3ff13929dffc61a5468e351
Konstantin Terekhin <studenttsu94@yandex.ru>
2019-09-24 03:21:04 +0700

    added categories list

--- src/js/main.js
+++ src/js/main.js
@@ -7,4 +7,60 @@ $(function () {
         slidesToScroll: 1
     });
 
+    var outsideClickListener = function (event) {
+        if ($(event.target).closest(".categories").length) return;
+
+        $('#catalog-btn').removeClass('active')
+            .find('.hamb').toggleClass('active');
+
+        $('.main-content').removeClass('overlay');
+        $('.categories').removeClass('opened');
+
+        $(document).off('click', outsideClickListener);
+        event.stopPropagation();
+    };
+
+    $('#catalog-btn').on('click', function (event) {
+        $(this).toggleClass('active');
+        $(this).find('.hamb').toggleClass('active');
+
+        $('.main-content').toggleClass('overlay');
+        $('.categories').toggleClass('opened');
+
+        if ($(this).hasClass('active')) {
+            event.stopPropagation();
+
+            initFirstCategory();
+            $(document).on('click', outsideClickListener);
+        }
+    });
+
+    var categoriesListItems = $('.categories-list li');
+    var categoryItems = $('.category-items');
+
+    categoriesListItems.on('mouseenter', function () {
+        var that = $(this);
+
+        if (!that.hasClass('active')) {
+            var id = that.data('id');
+
+            categoriesListItems.removeClass('active');
+            that.addClass('active');
+
+            categoryItems.removeClass('active');
+            $('.category-items[data-target=' + id + ']').addClass('active');
+        }
+    });
+
+    function initFirstCategory() {
+        var firstCategory = categoriesListItems.first();
+        var firstCategoryId = firstCategory.data('id');
+
+        categoriesListItems.removeClass('active');
+        firstCategory.addClass('active');
+
+        categoryItems.removeClass('active');
+        $('.category-items[data-target=' + firstCategoryId + ']').addClass('active');
+    }
+
 });

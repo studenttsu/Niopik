f452b08950c2fcc709172f80f98fc2ae15c4c17c
Konstantin Terekhin <studenttsu94@yandex.ru>
2019-09-24 11:23:37 +0700

    footer media styles

--- src/js/main.js
+++ src/js/main.js
@@ -4,8 +4,28 @@ $(function () {
 
     $('.docs-slider').slick({
         slidesToShow: 3,
-        slidesToScroll: 1
+        slidesToScroll: 1,
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
+
+    $('.header__navbar .hamb').on('click', function () {
+        $(this).toggleClass('active');
+    })
 
     var outsideClickListener = function (event) {
         if ($(event.target).closest(".categories").length) return;
